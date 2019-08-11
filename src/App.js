85c00421596c598179819e099f8983a9a04dfc72
phatbhoy67 <chris.scanlon72@gmail.com>
2019-08-11 12:28:34 +0100

    Added routes and switch for 404s

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
@@ -9,11 +10,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      Hello World!
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
