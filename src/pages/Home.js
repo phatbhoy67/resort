b5ce4b582977e65efad3fa378ec830d3edbc6271
phatbhoy67 <chris.scanlon72@gmail.com>
2019-08-12 15:26:25 +0100

    SingleRoom component completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import Services from "../components/Services";
 import FeaturedRooms from "../components/FeaturedRooms";
+
 export default function Home() {
   return (
     <>
