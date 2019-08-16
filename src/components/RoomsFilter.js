3e5fad3a5f469182d88549d8697d7afccb5b9655
phatbhoy67 <chris.scanlon72@gmail.com>
2019-08-16 15:35:01 +0100

    Switched data source from local to Contentful

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -76,7 +76,7 @@ export default function RoomsFilter({ rooms }) {
 
         {/* room price */}
         <div className="form-group">
-          <label htmlFor="price">room price ${price}</label>
+          <label htmlFor="price">room price £{price}</label>
           <input
             type="range"
             name="price"
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -65,7 +65,7 @@ export default class SingleRoom extends Component {
             </article>
             <article className="info">
               <h3>info</h3>
-              <h6>price: ${price}</h6>
+              <h6>price: £{price}</h6>
               <h6>size: {size} SQFT</h6>
               <h6>
                 max capacity:{" "}
